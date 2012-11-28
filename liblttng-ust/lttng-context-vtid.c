c1e82f74181f13b7fc893c2109320eb71b60e491
Christian Babeux <christian.babeux@efficios.com>
2012-11-27 23:01:06 -0500

    Typo: Conflict in lttng-context-vtid. not properly resolved
    The conflict that appeared on commit d9ecffa9ad6203df66b5fa81e8d505c3a9779c16
    was not properly resolved.

--- liblttng-ust/lttng-context-vtid.c
+++ liblttng-ust/lttng-context-vtid.c
@@ -28,26 +28,6 @@
 #include "ltt-tracer-core.h"
 #include <lttng/ust-tid.h>
 
-#ifdef __linux__
-#include <syscall.h>
-#endif
-
-#if defined(_syscall0)
-_syscall0(pid_t, gettid)
-#elif defined(__NR_gettid)
-static inline pid_t gettid(void)
-{
-	return syscall(__NR_gettid);
-}
-#else
-#warning "use pid as tid"
-static inline pid_t gettid(void)
-{
-	return getpid();
-}
-#endif
-======= end
-
 /*
  * We cache the result to ensure we don't trigger a system call for
  * each event.
